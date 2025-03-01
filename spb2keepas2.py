17a5923bee41cbc3280caa40e04a552ea45d2c27
Сергей Горбатенко <gorbatenkosy@ema.ru>
2025-03-01 22:47:08 +0700

    Finish first working version

--- spb2keepas2.py
+++ spb2keepas2.py
@@ -12,15 +12,13 @@ def parse_input_file(filename):
     current_entry = None  # Текущая запись
     notes_lines = []  # Буфер для строк Notes
     is_notes = False  # Флаг, что мы находимся в блоке Notes
-    notes_count = 0
-    position = 0
+    notes_line_count = 0
+    line_index = 0
     before_line = None
     for line in lines:
-        position += len(line)
         line = line.strip()
 
-        # Обработка пустых строк
-        if not line:
+        if notes_line_count == 0:
             if is_notes:
                 # Если это конец Notes, добавляем их в запись
                 current_entry['fields']['Notes'] = '\n'.join(notes_lines)
@@ -28,88 +26,62 @@ def parse_input_file(filename):
                 is_notes = False
                 current_group['entries'].append(current_entry)
                 current_entry = None
-            elif current_entry:
-                # Если это конец записи, добавляем её в текущую группу
-                current_group['entries'].append(current_entry)
-                current_entry = None
-            elif before_line:
-                if group_stack:
-                    # Если есть родительская группа, добавляем текущую группу в неё
-                    parent_group = current_group
-                    parent_group['groups'].append({'name': before_line, 'groups': [], 'entries': []})
-                    current_group = parent_group['groups'][-1]
-                else:
-                    # Иначе создаем новую группу
-                    current_group = {'name': before_line, 'groups': [], 'entries': []}
-                    group_stack.append(current_group)
-                # continue
             else:
-                # Если это конец группы, добавляем её в родительскую группу
-                if group_stack:
-                    # parent_group = group_stack[-1]
-                    # parent_group['groups'].append(current_group)
-                    # current_group = None
-                    current_group = parent_group
-                # else:
-                #     groups.append(current_group)
-                #     current_group = None
-            # continue
-
-        # Обработка новой группы
-        # if not current_entry:
-        #     if group_stack:
-        #         # Если есть родительская группа, добавляем текущую группу в неё
-        #         parent_group = group_stack[-1]
-        #         parent_group['groups'].append({'name': line, 'groups': [], 'entries': []})
-        #         current_group = parent_group['groups'][-1]
-        #     else:
-        #         # Иначе создаем новую группу
-        #         current_group = {'name': line, 'groups': [], 'entries': []}
-        #         group_stack.append(current_group)
-        #     continue
-
-        # Обработка новой записи
-        else:
-            if is_notes:
-                notes_lines.append(line)
-                # continue
-            else:
-            # Обработка строк с ключами
-                if ':' in line:
-                    if not current_entry :
-                        current_entry = {'title': before_line, 'fields': {}}
-                    
-                    else:
-                        key, value = line.split(':', 1)
-                        key = key.strip()
-                        value = value.strip()
-
-                        if key == 'Notes':
-                            is_notes = True
-                            match = re.match(r'(\n\n^[^:\n]+\n.+:\s.+)|(\n\n\n^[^:\n]+\n\n)',  lines[position:])
-                            if match:
-                                notes_end=match.start()+1
+                # Обработка пустых строк
+                if not line:
+                    if current_entry:
+                        # Если это конец записи, добавляем её в текущую группу
+                        current_group['entries'].append(current_entry)
+                        current_entry = None
+                    elif before_line:
+                        if group_stack:
+                            # Если есть родительская группа, добавляем текущую группу в неё
+                            parent_group = current_group
+                            parent_group['groups'].append({'name': before_line, 'groups': [], 'entries': []})
+                            current_group = parent_group['groups'][-1]
                         else:
-                            current_entry['fields'][key] = value
-                    # continue
+                            # Иначе создаем новую группу
+                            current_group = {'name': before_line, 'groups': [], 'entries': []}
+                            group_stack.append(current_group)
+                        # continue
+                    else:
+                        # Если это конец группы, добавляем её в родительскую группу
+                        if group_stack:
+                            current_group = parent_group
 
+                # Обработка новой записи
+                else:
+                    # Обработка строк с ключами
+                        if ':' in line:
+                            if not current_entry :
+                                current_entry = {'title': before_line, 'fields': {}}
+                            # else:
+                            key, value = line.split(':', 1)
+                            key = key.strip()
+                            value = value.strip()
+
+                            if key == 'Notes':
+                                is_notes = True
+                                notes_lines.append(value)
+                                
+                                rest_of_the_file = ''.join(lines[line_index:])
+                                match = re.search(r'(\n\n^[^:\n]+\n.+:\s.+)|(\n\n\n^[^:\n]+\n\n)',  rest_of_the_file, re.MULTILINE)
+                                if match:
+                                    notes_end=match.start()+1
+                                    notes_line_count = match.string[:notes_end].count('\n')-1
+                            else:
+                                current_entry['fields'][key] = value
+        else: 
+            notes_lines.append(line)
+            notes_line_count -= 1
+            
+        line_index += 1
         before_line = line
-
-    # Добавляем последнюю запись и группу, если они есть
-    # if current_entry:
-    #     if notes_lines:
-    #         current_entry['fields']['Notes'] = '\n'.join(notes_lines)
-    #     current_group['entries'].append(current_entry)
-    # if current_group:
-    #     if group_stack:
-    #         parent_group = group_stack[-1]
-    #         parent_group['groups'].append(current_group)
-    #     else:
-    #         groups.append(current_group)
 
     return group_stack
 
 def create_kp_xml(groups, parent_group_elem=None):
+    root = None
     if parent_group_elem is None:
         root = ET.Element('KeePassFile')
         meta = ET.SubElement(root, 'Meta')
@@ -125,8 +97,9 @@ def create_kp_xml(groups, parent_group_elem=None):
         # Обработка записей
         for entry in group['entries']:
             entry_elem = ET.SubElement(group_elem, 'Entry')
-            ET.SubElement(entry_elem, 'Key').text = 'Title'
-            ET.SubElement(entry_elem, 'Value').text = entry['title']
+            string_elem = ET.SubElement(entry_elem, 'String')
+            ET.SubElement(string_elem, 'Key').text = 'Title'
+            ET.SubElement(string_elem, 'Value').text = entry['title']
 
             for key, value in entry['fields'].items():
                 string_elem = ET.SubElement(entry_elem, 'String')
@@ -137,7 +110,7 @@ def create_kp_xml(groups, parent_group_elem=None):
         if group['groups']:
             create_kp_xml(group['groups'], group_elem)
 
-    return parent_group_elem
+    return root
 
 def save_xml(xml_root, filename):
     xml_str = ET.tostring(xml_root, encoding='utf-8')
