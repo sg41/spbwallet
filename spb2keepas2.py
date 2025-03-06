e498e149be915053b5744e0ca1a2f330be4711a7
Сергей Горбатенко <gorbatenkosy@ema.ru>
2025-03-06 21:24:26 +0700

    Refactoring

--- spb2keepas2.py
+++ spb2keepas2.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 import re
+import argparse
 
 def translate_key(key):
     translations = {
@@ -13,16 +14,15 @@ def translate_key(key):
     }
     return translations.get(key, key)
 
-def parse_input_file(filename):
-    with open(filename, 'r', encoding='utf-16') as f:
+def parse_input_file(filename, input_encoding='utf-16'):
+    with open(filename, 'r', encoding=input_encoding) as f:
         lines = f.readlines()
 
-    groups = []  # Список всех групп
-    group_stack = []  # Стек для вложенных групп
-    current_group = None  # Текущая группа
-    current_entry = None  # Текущая запись
-    notes_lines = []  # Буфер для строк Notes
-    is_notes = False  # Флаг, что мы находимся в блоке Notes
+    group_stack = []  # Stack for nested groups
+    current_group = None  # Current group
+    current_entry = None  # Current entry
+    notes_lines = []  # Buffer for Notes lines
+    is_notes = False  # Flag indicating if we are in a Notes block
     notes_line_count = 0
     line_index = 0
     before_line = None
@@ -31,61 +31,61 @@ def parse_input_file(filename):
 
         if notes_line_count == 0:
             if is_notes:
-                # Если это конец Notes, добавляем их в запись
+                # If it is the end of Notes, add them to the entry
                 current_entry['fields']['Notes'] = '\n'.join(notes_lines)
                 notes_lines = []
                 is_notes = False
+                # End of Notes always means the end of an entry
                 current_group['entries'].append(current_entry)
                 current_entry = None
             else:
-                # Обработка пустых строк
+                # Processing empty lines
                 if not line:
                     if current_entry:
-                        # Если это конец записи, добавляем её в текущую группу
+                        # If it is the end of an entry, add it to the current group
                         current_group['entries'].append(current_entry)
                         current_entry = None
-                    elif before_line:
+                    elif before_line: # non empty line followed by empty line is a group name
                         if group_stack:
-                            # Если есть родительская группа, добавляем текущую группу в неё
+                            # If there is a parent group, add the current group to it
                             parent_group = current_group
                             parent_group['groups'].append({'name': before_line, 'groups': [], 'entries': []})
                             current_group = parent_group['groups'][-1]
                         else:
-                            # Иначе создаем новую группу
+                            # Otherwise create a new group
                             current_group = {'name': before_line, 'groups': [], 'entries': []}
                             group_stack.append(current_group)
-                        # continue
                     else:
-                        # Если это конец группы, добавляем её в родительскую группу
+                        # If it is the end of a group
                         if group_stack:
                             current_group = parent_group
 
-                # Обработка новой записи
                 else:
-                    # Обработка строк с ключами
-                        if ':' in line:
-                            if not current_entry :
-                                current_entry = {'title': before_line, 'fields': {}}
-                            # else:
-                            key, value = line.split(':', 1)
-                            key = key.strip()
-                            value = value.strip()
+                    # Processing lines with keys
+                    if ':' in line:
+                        if not current_entry :
+                            # Create a new entry
+                            current_entry = {'title': before_line, 'fields': {}}
 
-                            if key == 'Notes':
-                                is_notes = True
-                                notes_lines.append(value)
-                                
-                                rest_of_the_file = ''.join(lines[line_index:])
-                                match = re.search(r'(\n\n^[^:\n]+\n.+:\s.+)|(\n\n\n^[^:\n]+\n\n)',  rest_of_the_file, re.MULTILINE)
-                                if match:
-                                    notes_end=match.start()+1
-                                    notes_line_count = match.string[:notes_end].count('\n')-1
-                            else:
-                                current_entry['fields'][translate_key(key)] = value
-        else: 
+                        key, value = line.split(':', 1)
+                        key = key.strip()
+                        value = value.strip()
+
+                        if key == 'Notes':
+                            is_notes = True
+                            notes_lines.append(value)
+
+                            rest_of_the_file = ''.join(lines[line_index:])
+                            match = re.search(r'(\n\n^[^:\n]+\n.+:\s.+)|(\n\n\n^[^:\n]+\n\n)',  rest_of_the_file, re.MULTILINE)
+                            if match:
+                                notes_end=match.start()+1
+                                notes_line_count = match.string[:notes_end].count('\n')-1
+                        else:
+                            current_entry['fields'][translate_key(key)] = value
+        else:
             notes_lines.append(line)
             notes_line_count -= 1
-            
+
         line_index += 1
         before_line = line
 
@@ -105,7 +105,7 @@ def create_kp_xml(groups, parent_group_elem=None):
         group_elem = ET.SubElement(parent_group_elem, 'Group')
         ET.SubElement(group_elem, 'Name').text = group['name']
 
-        # Обработка записей
+        # Entry processing
         for entry in group['entries']:
             entry_elem = ET.SubElement(group_elem, 'Entry')
             string_elem = ET.SubElement(entry_elem, 'String')
@@ -117,22 +117,30 @@ def create_kp_xml(groups, parent_group_elem=None):
                 ET.SubElement(string_elem, 'Key').text = key
                 ET.SubElement(string_elem, 'Value').text = value
 
-        # Рекурсивная обработка вложенных групп
+        # Recursive processing of nested groups
         if group['groups']:
             create_kp_xml(group['groups'], group_elem)
 
     return root
 
-def save_xml(xml_root, filename):
-    xml_str = ET.tostring(xml_root, encoding='utf-8')
+def save_xml(xml_root, filename, output_encoding='utf-8'):
+    xml_str = ET.tostring(xml_root, encoding=output_encoding)
     dom = minidom.parseString(xml_str)
-    pretty_xml = dom.toprettyxml(indent='  ', encoding='utf-8').decode('utf-8')
+    pretty_xml = dom.toprettyxml(indent='  ', encoding=output_encoding).decode(output_encoding)
 
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'w', encoding=output_encoding) as f:
         f.write(pretty_xml)
 
 if __name__ == '__main__':
-    groups = parse_input_file('input.txt')
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('input_file', nargs='?', default='input.txt')
+    argparser.add_argument('output_file', nargs='?', default='output.xml')
+    argparser.add_argument('--input_encoding', default='utf-16', help='Input file encoding')
+    argparser.add_argument('--output_encoding', default='utf-8', help='Output file encoding')
+    args = argparser.parse_args()
+
+    groups = parse_input_file(args.input_file, args.input_encoding)
     xml_root = create_kp_xml(groups)
-    save_xml(xml_root, 'keepass_output.xml')
-    print("Файл успешно преобразован в keepass_output.xml")
+    save_xml(xml_root, args.output_file, args.output_encoding)
+    
+    print(f"File {args.input_file} successfully converted to {args.output_file}")
